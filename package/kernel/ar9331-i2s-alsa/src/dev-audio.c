61ce4626c41cc3844f2d822df470a299e99dfd1b
radio <60122178+lzsbb@users.noreply.github.com>
2020-04-07 20:12:48 +0800

    Update dev-audio.c

--- package/kernel/ar9331-i2s-alsa/src/dev-audio.c
+++ package/kernel/ar9331-i2s-alsa/src/dev-audio.c
@@ -37,8 +37,8 @@ static struct platform_device ath79_pcm_device = {
 	.id		= -1,
 };
 
-static struct platform_device wm8727_codec = {
-	.name		= "wm8727",
+static struct platform_device qca-ap123-ak4430_codec = {
+	.name		= "qca-ap123-ak4430",
 	.id		= -1,
 };
 
@@ -46,7 +46,7 @@ void __init ath79_audio_device_register(void)
 {
 	platform_device_register(&ath79_i2s_device);
 	platform_device_register(&ath79_pcm_device);
-	platform_device_register(&wm8727_codec);
+	platform_device_register(&qca-ap123-ak4430_codec);
 }
 
 void __init ath79_audio_setup(void)
