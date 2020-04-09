20c43ebad8206cdf18c0ed8ffa269eb4a259bad5
radio <60122178+lzsbb@users.noreply.github.com>
2020-04-09 08:32:39 +0800

    Create common.h

---
+++ package/kernel/ar9331-i2s-alsa/src/common.h
@@ -0,0 +1,27 @@
+#ifndef __COMMON_H
+#define __COMMON_H
+
+// 基本常用头文件
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+#include <stdbool.h>
+
+// 全局错误码
+#include <errno.h>
+
+// 文件IO操作
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+// 数学库
+#include <math.h>
+
+// 目录
+#include <dirent.h>
+
+#endif
