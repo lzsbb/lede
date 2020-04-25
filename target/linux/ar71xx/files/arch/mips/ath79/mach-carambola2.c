a045bc5a78eaa8b532000febdc906b90452e7527
radio <60122178+lzsbb@users.noreply.github.com>
2020-04-25 04:59:30 +0000

    Update mach-carambola2.c

--- target/linux/ar71xx/files/arch/mips/ath79/mach-carambola2.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-carambola2.c
@@ -7,23 +7,6 @@
  *  under the terms of the GNU General Public License version 2 as published
  *  by the Free Software Foundation.
  */
-/*
- * qca-db120.c -- ALSA machine code for DB12x board ref design (and relatives)
- *
- * Copyright (c) 2013 The Linux Foundation. All rights reserved.
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
 
 #include <asm/mach-ath79/ath79.h>
 #include <asm/mach-ath79/ar71xx_regs.h>
@@ -36,16 +19,6 @@
 #include "dev-usb.h"
 #include "dev-wmac.h"
 #include "machtypes.h"
-
-#include <linux/types.h>
-#include <sound/core.h>
-#include <sound/soc.h>
-#include <linux/module.h>
-
-/* Driver include */
-
-#include "ath79-i2s.h"
-#include "ath79-pcm.h"
 
 #define CARAMBOLA2_GPIO_LED_WLAN		0
 #define CARAMBOLA2_GPIO_LED_ETH0		14
@@ -128,59 +101,5 @@ static void __init carambola2_setup(void)
 	ath79_register_usb();
 }
 
-
-
-
-
-static struct platform_device *carambola_snd_device;
-
-static struct snd_soc_dai_link carambola_dai = {
-	.name = "Carambola2 audio",
-	.stream_name = "Carambola2 audio",
-	.cpu_dai_name = "ath79-i2s",
-	.codec_dai_name = "ak4430-hifi",
-	.platform_name = "ath79-pcm-audio",
-	.codec_name = "ak4430-codec",
-	/* use ops to check startup state */
-};
-
-static struct snd_soc_card snd_soc_carambola = {
-	.name = "ath Carambola2",
-	.long_name = "Carambola2 - ath79-pcm/ath79-i2s/qca-ap123-ak4430",
-	.dai_link = &carambola_dai,
-	.num_links = 1,
-};
-
-static int __init carmabola2_init(void)
-{
-	int ret;
-
-	carambola_snd_device = platform_device_alloc("soc-audio", -1);
-	if(!carambola_snd_device)
-		return -ENOMEM;
-
-	platform_set_drvdata(carambola_snd_device, &snd_soc_carambola);
-	ret = platform_device_add(carambola_snd_device);
-
-	if (ret) {
-		platform_device_put(carambola_snd_device);
-	}
-
-	return ret;
-}
-
-static void __exit carmabola2_exit(void)
-{
-	platform_device_unregister(carambola_snd_device);
-}
-
-module_init(carmabola2_init);
-module_exit(carmabola2_exit);
-
 MIPS_MACHINE(ATH79_MACH_CARAMBOLA2, "CARAMBOLA2", "8devices Carambola2 board",
 		carambola2_setup);
-
-MODULE_AUTHOR("Qualcomm-Atheros Inc.");
-MODULE_AUTHOR("Mathieu Olivari <mathieu@qca.qualcomm.com>");
-MODULE_DESCRIPTION("QCA Audio Machine module");
-MODULE_LICENSE("Dual BSD/GPL");
